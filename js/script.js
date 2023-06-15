68e42ebfbb9b736d0ce3a2b6c58a0cddc3b625ae
Eros000 <e.ghilardi@hotmail.it>
2023-06-15 08:31:59 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 for (let number = 0 ; number <= 100 ; number ++){
+
+    console.log (number)
     
     if (number % 3 == 0){
 
-        number = ('Fizz')
-        console.log ('number', number, typeof number);
-    }
+        number='Fizz'
         
-    
-    
+        console.log (number);
+        
+    }
+    else if (number % 5 == 0){
+
+        number='Buzz'
+        
+        console.log (number);
+
+    }
+    else if (number % 3 == 0 && number % 5 == 0){
+        
+        console.log (number, 'FizzBuzz');
+        
+    }
 }
 
 
